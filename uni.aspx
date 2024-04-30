bfcec487f1fefc6faa52f00844ef4cff47239d0f
Emre Kaya <133666788+Excalibur312@users.noreply.github.com>
2024-04-30 12:13:45 +0300

    Add files via upload

--- uni.aspx
+++ uni.aspx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="uni.aspx.cs" Inherits="CampusAtlas.uni" %>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,30 +17,3 @@
     </form>
 </body>
 </html>
-=======
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="uni.aspx.cs" Inherits="CampusAtlas.uni" %>
-
-<!DOCTYPE html>
-<html lang="en">
-<head runat="server">
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
-</head>
-<body>
-    <form id="form1" runat="server">
-        <div>
-            <asp:Literal ID="litUniversityData" runat="server" />
-        </div>
-    </form>
-    <script>
-        function goToDepartmentPage(pageNo) {
-            var url = "Department.aspx?pageNo=" + encodeURIComponent(pageNo);
-            window.open(url, "_blank");
-        }
-    </script>
-</body>
-</html>
->>>>>>> 16655f4c31da23fa686c127c1fcea6ba863946c5
--- uni.aspx.cs
+++ uni.aspx.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Data.SqlClient;
 
@@ -56,67 +55,3 @@ namespace CampusAtlas
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Data;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-
-namespace CampusAtlas
-{
-    public partial class uni : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            string location = Request.QueryString["location"];
-            string servername = "localhost";  // Sunucu adı
-            string username = "root";  // Veritabanı kullanıcı adı
-            string password = "root";  // Veritabanı şifresi
-            string dbname = "universities";  // Veritabanı adı
-
-            using (var conn = new SqlConnection("Data Source=" + servername + ";Initial Catalog=" + dbname + ";User ID=" + username + ";Password=" + password))
-            {
-                conn.Open();
-
-                if (conn.State == ConnectionState.Open)
-                {
-                    using (var cmd = new SqlCommand("SELECT * FROM uni WHERE location LIKE @Location", conn))
-                    {
-                        cmd.Parameters.AddWithValue("@Location", "%" + location + "%");
-
-                        using (var reader = cmd.ExecuteReader())
-                        {
-                            if (reader.HasRows)
-                            {
-                                litUniversityData.Text = "<table class='table table-striped'><thead class='thead-dark' style='height: 5rem;'><tr><th class='col-8 text-center font-weight-bold'>Üniversite Adı</th><th class='col-4 font-weight-bold'>Bölüm</th></tr></thead><tbody>";
-
-                                while (reader.Read())
-                                {
-                                    string name = reader["name"].ToString();
-                                    string pageNo = reader["pageNo"].ToString();
-
-                                    litUniversityData.Text += "<tr style='height: 5rem;'><td class='font-weight-bold col-8 align-middle' style='padding-left: 18%;'>" + name + "</td><td class='align-middle'><button class='btn btn-info col-4' onclick='goToDepartmentPage(\"" + pageNo + "\")'>Bölümler</button></td></tr>";
-                                }
-
-                                litUniversityData.Text += "</tbody></table>";
-                            }
-                            else
-                            {
-                                litUniversityData.Text = "There is no available data";
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    litUniversityData.Text = "Veritabanına bağlanılamadı.";
-                }
-            }
-        }
-    }
-}
->>>>>>> 16655f4c31da23fa686c127c1fcea6ba863946c5
