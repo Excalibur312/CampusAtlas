a7b0187d0e71c8156ec32c714af38ea562be2707
Esma Nur Arslan <96125581+esmanurarslan@users.noreply.github.com>
2024-04-30 21:59:08 +0300

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -4,164 +4,164 @@
         region  varchar(20) 
     );
     
-    INSERT INTO city (id, name,region) VALUES (1, 'Adana','Akdeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(2, 'Adiyaman','Güneydoğu Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(3, 'Afyonkarahisar','Ege');
-    
-    INSERT INTO city (id, name,region) VALUES(4, 'Agri','Doğu Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(5, 'Amasya','Karadeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(6, 'Ankara','İç Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(7, 'Antalya','Akdeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(8, 'Artvin','Karadeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(9, 'Aydin','Ege');
-    
-    INSERT INTO city (id, name,region) VALUES(10, 'Balikesir','Marmara');
-    
-    INSERT INTO city (id, name,region) VALUES(11, 'Bilecik','Marmara');
-    
-    INSERT INTO city (id, name,region) VALUES(12, 'Bingol','Doğu Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(13, 'Bitlis','Doğu Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(14, 'Bolu','Karadeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(15, 'Burdur','Akdeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(16, 'Bursa','Marmara');
-    
-    INSERT INTO city (id, name,region) VALUES(17, 'Canakkale','Marmara');
-    
-    INSERT INTO city (id, name,region) VALUES(18, 'Cankiri','İç Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(19, 'Corum','Karadeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(20, 'Denizli','Ege');
-    
-    INSERT INTO city (id, name,region) VALUES(21, 'Diyarbakir','Güneydoğu Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(22, 'Edirne','Marmara');
-    
-    INSERT INTO city (id, name,region) VALUES(23, 'Elazig','Doğu Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(24, 'Erzincan','Doğu Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(25, 'Erzurum','Doğu Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(26, 'Eskisehir','İç Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(27, 'Gaziantep','Güneydoğu Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(28, 'Giresun','Karadeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(29, 'Gumushane','Karadeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(30, 'Hakkari','Doğu Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(31, 'Hatay','Akdeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(32, 'Isparta','Akdeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(33, 'Mersin','Akdeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(34, 'Istanbul','Marmara');
-    
-    INSERT INTO city (id, name,region) VALUES(35, 'Izmir','Ege');
-    
-    INSERT INTO city (id, name,region) VALUES(36, 'Kars','Doğu Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(37, 'Kastamonu','Karadeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(38, 'Kayseri','İç Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(39, 'Kirklareli','Marmara');
-    
-    INSERT INTO city (id, name,region) VALUES(40, 'Kirsehir','İç Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(41, 'Kocaeli','Marmara');
-    
-    INSERT INTO city (id, name,region) VALUES(42, 'Konya','İç Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(43, 'Kutahya','Ege');
-    
-    INSERT INTO city (id, name,region) VALUES(44, 'Malatya','Doğu Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(45, 'Manisa','Ege');
-    
-    INSERT INTO city (id, name,region) VALUES(46, 'Kahramanmaras','Akdeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(47, 'Mardin','Güneydoğu Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(48, 'Mugla','Ege');
-    
-    INSERT INTO city (id, name,region) VALUES(49, 'Mus','Doğu Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(50, 'Nevsehir','İç Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(51, 'Nigde','İç Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(52, 'Ordu','Karadeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(53, 'Rize','Karadeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(54, 'Sakarya','Marmara');
-    
-    INSERT INTO city (id, name,region) VALUES(55, 'Samsun','Karadeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(56, 'Siirt','Güneydoğu Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(57, 'Sinop','Karadeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(58, 'Sivas','İç Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(59, 'Tekirdag','Marmara');
-    
-    INSERT INTO city (id, name,region) VALUES(60, 'Tokat','Karadeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(61, 'Trabzon','Karadeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(62, 'Tunceli','Doğu Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(63, 'Sanliurfa','Güneydoğu Anadolu ');
-    
-    INSERT INTO city (id, name,region) VALUES(64, 'Usak','Ege');
-    
-    INSERT INTO city (id, name,region) VALUES(65, 'Van','Doğu Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(66, 'Yozgat','İç Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(67, 'Zonguldak','Karadeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(68, 'Aksaray','İç Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(69, 'Bayburt','Karadeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(70, 'Karaman','İç Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(71, 'Kirikkale','İç Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(72, 'Batman','Güneydoğu Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(73, 'Sirnak','Güneydoğu Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(74, 'Bartin','Karadeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(75, 'Ardahan','Doğu Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(76, 'Igdir','Doğu Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(77, 'Yalova','Marmara');
-    
-    INSERT INTO city (id, name,region) VALUES(78, 'Karabuk','Karadeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(79, 'Kilis','Güneydoğu Anadolu');
-    
-    INSERT INTO city (id, name,region) VALUES(80, 'Osmaniye','Akdeniz');
-    
-    INSERT INTO city (id, name,region) VALUES(81, 'Duzce','Karadeniz');
+INSERT INTO city (id, name,region) VALUES (1, 'Adana','Akdeniz');
+
+INSERT INTO city (id, name,region) VALUES(2, 'Adıyaman','Güneydoğu Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(3, 'Afyonkarahisar','Ege');
+
+INSERT INTO city (id, name,region) VALUES(4, 'Ağrı','Doğu Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(5, 'Amasya','Karadeniz');
+
+INSERT INTO city (id, name,region) VALUES(6, 'Ankara','İç Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(7, 'Antalya','Akdeniz');
+
+INSERT INTO city (id, name,region) VALUES(8, 'Artvin','Karadeniz');
+
+INSERT INTO city (id, name,region) VALUES(9, 'Aydın','Ege');
+
+INSERT INTO city (id, name,region) VALUES(10, 'Balıkesir','Marmara');
+
+INSERT INTO city (id, name,region) VALUES(11, 'Bilecik','Marmara');
+
+INSERT INTO city (id, name,region) VALUES(12, 'Bingöl','Doğu Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(13, 'Bitlis','Doğu Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(14, 'Bolu','Karadeniz');
+
+INSERT INTO city (id, name,region) VALUES(15, 'Burdur','Akdeniz');
+
+INSERT INTO city (id, name,region) VALUES(16, 'Bursa','Marmara');
+
+INSERT INTO city (id, name,region) VALUES(17, 'Çanakkale','Marmara');
+
+INSERT INTO city (id, name,region) VALUES(18, 'Çankırı','İç Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(19, 'Çorum','Karadeniz');
+
+INSERT INTO city (id, name,region) VALUES(20, 'Denizli','Ege');
+
+INSERT INTO city (id, name,region) VALUES(21, 'Diyarbakır','Güneydoğu Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(22, 'Edirne','Marmara');
+
+INSERT INTO city (id, name,region) VALUES(23, 'Elazığ','Doğu Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(24, 'Erzincan','Doğu Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(25, 'Erzurum','Doğu Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(26, 'Eskişehir','İç Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(27, 'Gaziantep','Güneydoğu Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(28, 'Giresun','Karadeniz');
+
+INSERT INTO city (id, name,region) VALUES(29, 'Gümüşhane','Karadeniz');
+
+INSERT INTO city (id, name,region) VALUES(30, 'Hakkari','Doğu Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(31, 'Hatay','Akdeniz');
+
+INSERT INTO city (id, name,region) VALUES(32, 'Isparta','Akdeniz');
+
+INSERT INTO city (id, name,region) VALUES(33, 'Mersin','Akdeniz');
+
+INSERT INTO city (id, name,region) VALUES(34, 'İstanbul','Marmara');
+
+INSERT INTO city (id, name,region) VALUES(35, 'İzmir','Ege');
+
+INSERT INTO city (id, name,region) VALUES(36, 'Kars','Doğu Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(37, 'Kastamonu','Karadeniz');
+
+INSERT INTO city (id, name,region) VALUES(38, 'Kayseri','İç Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(39, 'Kırklareli','Marmara');
+
+INSERT INTO city (id, name,region) VALUES(40, 'Kırşehir','İç Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(41, 'Kocaeli','Marmara');
+
+INSERT INTO city (id, name,region) VALUES(42, 'Konya','İç Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(43, 'Kütahya','Ege');
+
+INSERT INTO city (id, name,region) VALUES(44, 'Malatya','Doğu Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(45, 'Manisa','Ege');
+
+INSERT INTO city (id, name,region) VALUES(46, 'Kahramanmaraş','Akdeniz');
+
+INSERT INTO city (id, name,region) VALUES(47, 'Mardin','Güneydoğu Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(48, 'Muğla','Ege');
+
+INSERT INTO city (id, name,region) VALUES(49, 'Muş','Doğu Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(50, 'Nevşehir','İç Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(51, 'Niğde','İç Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(52, 'Ordu','Karadeniz');
+
+INSERT INTO city (id, name,region) VALUES(53, 'Rize','Karadeniz');
+
+INSERT INTO city (id, name,region) VALUES(54, 'Sakarya','Marmara');
+
+INSERT INTO city (id, name,region) VALUES(55, 'Samsun','Karadeniz');
+
+INSERT INTO city (id, name,region) VALUES(56, 'Siirt','Güneydoğu Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(57, 'Sinop','Karadeniz');
+
+INSERT INTO city (id, name,region) VALUES(58, 'Sivas','İç Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(59, 'Tekirdağ','Marmara');
+
+INSERT INTO city (id, name,region) VALUES(60, 'Tokat','Karadeniz');
+
+INSERT INTO city (id, name,region) VALUES(61, 'Trabzon','Karadeniz');
+
+INSERT INTO city (id, name,region) VALUES(62, 'Tunceli','Doğu Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(63, 'Şanlıurfa','Güneydoğu Anadolu ');
+
+INSERT INTO city (id, name,region) VALUES(64, 'Uşak','Ege');
+
+INSERT INTO city (id, name,region) VALUES(65, 'Van','Doğu Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(66, 'Yozgat','İç Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(67, 'Zonguldak','Karadeniz');
+
+INSERT INTO city (id, name,region) VALUES(68, 'Aksaray','İç Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(69, 'Bayburt','Karadeniz');
+
+INSERT INTO city (id, name,region) VALUES(70, 'Karaman','İç Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(71, 'Kırıkkale','İç Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(72, 'Batman','Güneydoğu Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(73, 'Şırnak','Güneydoğu Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(74, 'Bartın','Karadeniz');
+
+INSERT INTO city (id, name,region) VALUES(75, 'Ardahan','Doğu Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(76, 'Iğdır','Doğu Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(77, 'Yalova','Marmara');
+
+INSERT INTO city (id, name,region) VALUES(78, 'Karabük','Karadeniz');
+
+INSERT INTO city (id, name,region) VALUES(79, 'Kilis','Güneydoğu Anadolu');
+
+INSERT INTO city (id, name,region) VALUES(80, 'Osmaniye','Akdeniz');
+
+INSERT INTO city (id, name,region) VALUES(81, 'Düzce','Karadeniz');
