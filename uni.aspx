c84a75828a11c5964065cff3d94a1c1c07498784
Emre Kaya <133666788+Excalibur312@users.noreply.github.com>
2024-04-30 11:41:05 +0300

    Add files via upload

--- uni.aspx
+++ uni.aspx
@@ -1,3 +1,24 @@
+<<<<<<< HEAD
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="uni.aspx.cs" Inherits="CampusAtlas.uni" %>
+<!DOCTYPE html>
+<html lang="en">
+<head runat="server">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="locationLabel" runat="server" Text=""></asp:Label>
+        <asp:ScriptManager runat="server"></asp:ScriptManager>
+        <asp:TextBox ID="locationTextBox" runat="server"></asp:TextBox>
+        <div runat="server" id="resultDiv"></div>
+    </form>
+</body>
+</html>
+=======
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="uni.aspx.cs" Inherits="CampusAtlas.uni" %>
 
 <!DOCTYPE html>
@@ -23,3 +44,4 @@
     </script>
 </body>
 </html>
+>>>>>>> 16655f4c31da23fa686c127c1fcea6ba863946c5
--- uni.aspx.cs
+++ uni.aspx.cs
@@ -1,3 +1,62 @@
+<<<<<<< HEAD
+﻿using System;
+using System.Data.SqlClient;
+
+namespace CampusAtlas
+{
+    public partial class uni : System.Web.UI.Page
+    {
+        string location = "";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Request.QueryString["location"] != null)
+                {
+                    location = Request.QueryString["location"];
+                    ListUniByLocation();
+                }
+            }
+        }
+
+        private void ListUniByLocation()
+        {
+            string connectionString = "Data Source=JETAIME;Initial Catalog=CampusAtlas;Integrated Security=True;Connect Timeout=30;Encrypt=False;MultiSubnetFailover=False";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("SELECT id, name, location, type, url FROM dbo.uni WHERE location LIKE @Location", connection);
+                command.Parameters.AddWithValue("@Location", "%" + location + "%");
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    resultDiv.InnerHtml = "<table class='table table-striped'><thead class='thead-dark' style='height: 5rem;'><tr><th class='col-2 text-center font-weight-bold'>ID</th><th class='col-3 text-center font-weight-bold'>Name</th><th class='col-3 text-center font-weight-bold'>Location</th><th class='col-2 text-center font-weight-bold'>Type</th><th class='col-2 text-center font-weight-bold'>URL</th></tr></thead><tbody>";
+
+                    while (reader.Read())
+                    {
+                        string id = reader["id"].ToString();
+                        string name = reader["name"].ToString();
+                        string loc = reader["location"].ToString();
+                        string type = reader["type"].ToString();
+                        string url = reader["url"].ToString();
+
+                        resultDiv.InnerHtml += "<tr><td class='text-center'>" + id + "</td><td class='text-center'>" + name + "</td><td class='text-center'>" + loc + "</td><td class='text-center'>" + type + "</td><td class='text-center'>" + url + "</td></tr>";
+                    }
+
+                    resultDiv.InnerHtml += "</tbody></table>";
+                }
+                else
+                {
+                    resultDiv.InnerHtml = "There is no available data";
+                }
+            }
+        }
+    }
+}
+=======
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -60,3 +119,4 @@ namespace CampusAtlas
         }
     }
 }
+>>>>>>> 16655f4c31da23fa686c127c1fcea6ba863946c5
