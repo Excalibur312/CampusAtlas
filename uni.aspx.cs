264efd0706ec760d503cf4eb80d0d498ee87565a
Emre Kaya <133666788+Excalibur312@users.noreply.github.com>
2024-05-01 17:03:19 +0300

    Add files via upload

--- uni.aspx.cs
+++ uni.aspx.cs
@@ -24,7 +24,7 @@ namespace CampusAtlas
             string connectionString = "Data Source=JETAIME;Initial Catalog=CampusAtlas;Integrated Security=True;Connect Timeout=30;Encrypt=False;MultiSubnetFailover=False";
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                SqlCommand command = new SqlCommand("SELECT id, name, location, type, url FROM dbo.uni WHERE location LIKE @Location", connection);
+                SqlCommand command = new SqlCommand("SELECT name, type, url FROM uni WHERE location LIKE @Location", connection);
                 command.Parameters.AddWithValue("@Location", "%" + location + "%");
 
                 connection.Open();
@@ -32,17 +32,17 @@ namespace CampusAtlas
 
                 if (reader.HasRows)
                 {
-                    resultDiv.InnerHtml = "<table class='table table-striped'><thead class='thead-dark' style='height: 5rem;'><tr><th class='col-2 text-center font-weight-bold'>ID</th><th class='col-3 text-center font-weight-bold'>Name</th><th class='col-3 text-center font-weight-bold'>Location</th><th class='col-2 text-center font-weight-bold'>Type</th><th class='col-2 text-center font-weight-bold'>URL</th></tr></thead><tbody>";
+                    resultDiv.InnerHtml = "<table class='table table-striped'><thead class='thead-dark' style='height: 5rem;'><tr><th class='col-3 text-center font-weight-bold'>Name</th><th class='col-3 text-center font-weight-bold'>Type</th></tr></thead><tbody>";
 
                     while (reader.Read())
                     {
-                        string id = reader["id"].ToString();
                         string name = reader["name"].ToString();
-                        string loc = reader["location"].ToString();
                         string type = reader["type"].ToString();
-                        string url = reader["url"].ToString();
 
-                        resultDiv.InnerHtml += "<tr><td class='text-center'>" + id + "</td><td class='text-center'>" + name + "</td><td class='text-center'>" + loc + "</td><td class='text-center'>" + type + "</td><td class='text-center'>" + url + "</td></tr>";
+                        // Üniversite ismi için bir bağlantı oluştur
+                        string universityLink = "<a href='" + reader["url"].ToString() + "' target='_blank'>" + name + "</a>";
+
+                        resultDiv.InnerHtml += "<tr><td class='text-center'>" + universityLink + "</td><td class='text-center'>" + type + "</td></tr>";
                     }
 
                     resultDiv.InnerHtml += "</tbody></table>";
